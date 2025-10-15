b6610fd523b3406f3198834e1d6257ea25a3e841
vatsaldhamanwala <dhamanwalavatsal@gamil.com>
2025-10-15 19:02:47 +0530

    add configuration for media files using multer and cloudinary packages.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //uploading file on cloudinary
+    const responseResult = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto',
+    });
+    console.log('🚀 ~ uploadOnCloudinary ~ responseResult:', responseResult.url);
+
+    return responseResult;
+  } catch (error) {
+    // when fie upload operations failed it remove the local saved temporary file
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
