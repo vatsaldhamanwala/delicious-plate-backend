b08a58890244a0ef043b096b6ce20eb4f802fd69
vatsaldhamanwala <dhamanwalavatsal@gamil.com>
2025-10-16 18:16:39 +0530

    resolve error.

--- src/utils/response-generators.js
+++ src/utils/response-generators.js
@@ -5,10 +5,10 @@ import { StatusCodes } from 'http-status-codes';
 export function responseGenerators(responseData, responseStatusCode, responseStatusMessage, responseError) {
   const responseJson = {};
 
-  responseJson?.data = responseData || null;
-  responseJson?.statusCode = responseStatusCode || StatusCodes.OK;
-  responseJson?.message = responseStatusMessage || 'OK';
-  responseJson?.error = responseError === undefined ? [] : responseError;
+  responseJson.data = responseData || null;
+  responseJson.statusCode = responseStatusCode || StatusCodes.OK;
+  responseJson.message = responseStatusMessage || 'OK';
+  responseJson.error = responseError === undefined ? [] : responseError;
 
   return responseJson;
 }
